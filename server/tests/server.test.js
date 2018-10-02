af503693176a4416c7210d1175bdb1fc3f6d6481
Vanessa White <whitevanessa14@gmail.com>
2018-10-01 22:12:44 -0400

    Add tests fpr GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 // testing lifecycle method - runs code before test case
 //only moves on to test case when we call done
 beforeEach((done) => {
-  Todo.remove({}).then(() => done()); // wipes all of the todos
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -27,7 +35,7 @@ describe('POST /todos', () => {
         }
 
         //checking the database is storing correctly
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -48,9 +56,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
